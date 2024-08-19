d70ed3cfcdc50a4965e7acc5c037abd7acec2b9e
Armine Martirosyan <armine.marti@hotmail.com>
2024-08-19 21:50:50 +0100

    5 chapter

--- src/homework/dynamicarray/DynamicArray.java
+++ src/homework/dynamicarray/DynamicArray.java
@@ -23,6 +23,7 @@ public class DynamicArray {
         int[] biggerArray = new int[array.length+10];
         for (int i = 0; i<array.length; i++){
             biggerArray[i] = array[i];
+            //we can use here arraycopy method//
         }
         array = biggerArray;
 
@@ -31,18 +32,69 @@ public class DynamicArray {
     //եթե տրված ինդեքսը մեր ունեցած մասիվի ինդեքսի սահմաններում է, վերադարձնել
     // մասիվի index-երրորդ էլեմենտը։ Հակառակ դեպքում վերադարձնել -1։
     public int getByIndex(int index) {
-        if (index<array.length){
-            return array[index];
+        if (index < 0 || index > size) {
+            return -1;
         } else
-        return -1;
+            return array[index];
+
+
+    }
+    public void deleteByIndex(int index){
+        if (index<0|| index>size){
+            System.out.println("wrong index");
+            return;
+        }
+        for (int i = index+1; i<size;i++){
+            array[i-1] = array[i];
+        }
+        size--;
     }
 
+    public void set(int index, int value){
+        if (index<0||index>size){
+            System.out.println("wrong index");
+            return;
+        }
+        array[index] = value;
+
+    }
+    public void add(int index, int value){
+        if(index<0||index>size){
+            System.out.println("wrong index");
+            return;
+        }
+        if (array.length==size){
+            extend();
+        }
+        for(int i = size; i>=index; i--){
+            array[index+1]= array[i];
+        }
+        array[index] = value;
+        size++;
+    }
+
+    public int getIndexByValue(int value){
+        for(int i = 0; i<size;i++){
+            if(array[i]==value){
+                return i;
+            }
+        }return -1;
+    }
+
+    public boolean exists(int value){
+        for(int i = 0;i<size;i++){
+            if(array[i] == value){
+                return true;
+            }
+        }return false;
+    }
     //տպել մասիվի ավելացված էլեմենտները
     public void print() {
-        for(int i = 0; i<array.length;i++){
-            System.out.println(array[i] + " ");
+        for(int i = 0; i<size;i++){
+            System.out.print(array[i] + " ");
         }
     }
+
 
 
 }
--- src/homework/dynamicarray/DynamicArrayDemo.java
+++ src/homework/dynamicarray/DynamicArrayDemo.java
@@ -7,10 +7,10 @@ public class DynamicArrayDemo {
 
         //checking if array extends automatically when we add more than 10 elements
         arrayOne.add(3);
-        arrayOne.add(13);
-        arrayOne.add(89);
-        arrayOne.add(27);
+        arrayOne.add(1);
+        arrayOne.add(9);
         arrayOne.add(7);
+        arrayOne.add(5);
         arrayOne.add(54);
         arrayOne.add(38);
         arrayOne.add(51);
@@ -27,9 +27,25 @@ public class DynamicArrayDemo {
 
         //trying method print
         arrayOne.print();
+        arrayOne.deleteByIndex(1);
+        arrayOne.print();
 
         //trying getbyIndex which is more than array.length
-        arrayOne.getByIndex(40);
-        System.out.println(arrayOne.getByIndex(40));
+        arrayOne.getByIndex(4);
+        System.out.println(arrayOne.getByIndex(4));
+
+arrayOne.set(0,5);
+arrayOne.print();
+        System.out.println();
+arrayOne.add(4,33);
+        System.out.println();
+arrayOne.print();
+        System.out.println();
+        arrayOne.exists(0);
+        System.out.println(arrayOne.exists(6));
+        System.out.println();
+        arrayOne.getIndexByValue(5);
+        System.out.println(arrayOne.getIndexByValue(5));
+
     }
 }
