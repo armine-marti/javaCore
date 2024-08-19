242e3896d74d5dd47335716c3f89c61370a493ed
Armine Martirosyan <armine.marti@hotmail.com>
2024-08-19 20:31:50 +0100

    5 chapter

---
+++ src/classwork/chapter6/BoxDemo2.java
@@ -0,0 +1,23 @@
+package classwork.chapter6;
+
+public class BoxDemo2 {
+    public static void main(String[] args) {
+        Box mybox1 = new Box();
+        Box mybox2 = new Box();
+        double vol;
+         //assign values to mybox1's instance variable
+        mybox1.width = 10;
+        mybox1.height = 20;
+        mybox1.depth = 15;
+
+        mybox2.height = 6;
+        mybox2.depth = 9;
+        mybox2.width = 3;
+
+        vol = mybox1.width * mybox1.height * mybox1.depth;
+        System.out.println("Volume is " + vol);
+
+        vol = mybox2.width * mybox2.height * mybox2.depth;
+        System.out.println("Volume is " + vol);
+    }
+}
